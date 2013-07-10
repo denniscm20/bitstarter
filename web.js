c301e776c867cacd53e196fb729d7938f44a2bbe
Ubuntu <ubuntu@ip-10-165-31-253.ec2.internal>
2013-07-10 04:57:22 +0000

    Updated web.js - Read content from index.html

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html')
-  var content = buffer.toString('utf-8')
+  var buffer = fs.readFileSync('index.html');
+  var content = buffer.toString('utf-8');
   response.send(content);
 });
 
